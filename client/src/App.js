8a7fe4aa07a5e67749e76207bd838ba50cb6940f
Naser <nasersoftcorporation2013@gmail.com>
2020-05-09 18:01:33 +0600

    Initial client side setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className='text-warning'>Home</h1>
+      <button className='btn btn-secondary'>Test button</button>
+    </div>
+  );
+}
+
+export default App;

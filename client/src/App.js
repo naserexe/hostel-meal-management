008b8e2a9aa08ef3776c025af5f80016a1678dbe
Naser <nasersoftcorporation2013@gmail.com>
2020-05-09 18:58:15 +0600

    Navbar addeed

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
+import { Navbar } from './components/layout/Navbar';
 
 function App() {
   return (
-    <div className="container">
-      <h1 className='text-warning'>Home</h1>
-      <button className='btn btn-secondary'>Test button</button>
-    </div>
+    <Navbar/>
   );
 }
 

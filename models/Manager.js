2e7aaefb6ee8c8dc42c5337ca814614dd9e7f63b
Naser <nasersoftcorporation2013@gmail.com>
2020-05-09 17:02:32 +0600

    Fix some eslint error

--- models/Manager.js
+++ models/Manager.js
@@ -12,6 +12,7 @@ const ManagerSchema = new mongoose.Schema({
     required: [true, 'Please enter your email'],
     unique: true,
     match: [
+      // eslint-disable-next-line
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       'Please use correct email',
     ],
@@ -27,6 +28,7 @@ const ManagerSchema = new mongoose.Schema({
     required: [true, 'Please add hostel name'],
     // eslint-disable-next-line
     minlength: [5, 'hostel name should be more than 5 character'],
+    // eslint-disable-next-line
     match: [/^[a-zA-Z0-9\-]+$/, 'Your hostel name is not valid. Only characters A-Z, a-z, 0-9 and - are acceptable.'],
     unique: true,
   },

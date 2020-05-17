235a6cc3e95fb7f0d9d605ce07fa9fb8c423aea2
Naser <nasersoftcorporation2013@gmail.com>
2020-05-17 17:19:51 +0600

    Connect front and backend

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 
@@ -37,6 +38,15 @@ app.use('/api/test', test);
 
 
 const PORT = process.env.PORT || 5000;
+
+// Serve static asset in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
+
 
 // Error handler
 app.use(errorHandler);

494a271ff2ded8a34c1958f62075bce2a69caf3b
naser <abdnaser.exe@gmail.com>
2020-07-16 15:40:38 +0600

    deposit money to the member added

--- controllers/depositController.js
+++ controllers/depositController.js
@@ -12,7 +12,17 @@ exports.deposit = asyncHandler(async (req, res, next) => {
     if(!req.body.depositAmount || !req.body.user_id){
         return next(new ErrorResponse('Deposit amount and user_id is required', 401));
     }
-    const depositedUser = await User.updateOne({'hostelName': req.user.hostelName, '_id': req.body.user_id}, {$inc: {depositAmount: req.body.depositAmount}});
+    // const depositedUser = await User.findById(req.body.user_id);
+
+    // if(depositedUser.hostelName != req.user.hostelName){
+    //     return next(new ErrorResponse('Member not found with the given id', 404))
+    // }
+
+    // await depositedUser.updateOne({$inc: {depositAmount: req.body.depositAmount}}, {new: true});
+
+    const depositedUser = await User.findOneAndUpdate({_id: req.body.user_id, hostelName: req.user.hostelName}, {$inc: {depositAmount: req.body.depositAmount}},{new: true});
+
+    if(!depositedUser) return next(new ErrorResponse('Your member not found with the given id', 404))
 
     return res.status(200).json({success: true, data: depositedUser});
 });
